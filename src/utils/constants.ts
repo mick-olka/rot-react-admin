dcec1a8a757d5403f157bffeba6ab763bd74d108
mick-olka <mickolka64@gmail.com>
2023-09-17 19:11:03 +0300

    api_url upd

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const products_page_limit = 20
 export const orders_page_limit = 20
-export const API_URL = 'http://localhost:4000/api'
-export const PHOTOS_URL = 'http://localhost:4000/api/upload/'
+export const API_URL = 'http://213.217.8.92:4000/api'
+export const PHOTOS_URL = API_URL + '/upload/'
