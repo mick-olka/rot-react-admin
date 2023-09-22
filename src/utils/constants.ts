b34ec222b0614e7613f477f73c2611705eb98b96
mick-olka <mickolka64@gmail.com>
2023-09-22 16:59:18 +0300

    urls upd

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const products_page_limit = 40
 export const orders_page_limit = 20
 // export const API_URL = 'http://213.217.8.92:4000/api'
-export const API_URL = 'http://192.168.1.119:4000/api'
-export const client_url = 'http://192.168.1.119:3000/'
+export const API_URL = 'https://rotang.ua/api'
+export const client_url = 'https://rotang.ua/'
 export const PHOTOS_URL = API_URL + '/upload/'
